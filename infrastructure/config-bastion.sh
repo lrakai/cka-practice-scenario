e664b010000465529a28b567551ca0de2c852264
Logan Rakai <lmrakai@gmail.com>
2019-05-11 13:32:09 -0600

    Add logging

--- infrastructure/config-bastion.sh
+++ infrastructure/config-bastion.sh
@@ -1,6 +1,9 @@
 # Inject kubeconfig
-apt install sshpass
+env > /tmp/env
+apt-get install sshpass > /tmp/install_log 2>&1
 bastion_ip=$(aws ec2 describe-instances --region us-west-2 --filters "Name=tag:Name,Values=bastion*" "Name=instance-state-name,Values=pending,running" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-ssh-keyscan $bastion_ip >> ~/.ssh/known_hosts
-echo "put -P /home/ubuntu/.kube/config /home/ubuntu/.kube/config" > batch
-sshpass -p '-veryV3RY53CuRePazWurd!_' sftp -oBatchMode=no -b batch ubuntu@$bastion_ip
+echo $bastion_ip > /tmp/bastion_ip
+echo ~/.ssh/known_hosts > /tmp/known_hosts_path
+ssh-keyscan $bastion_ip >> ~/.ssh/known_hosts 2> /tmp/known_hosts_log
+echo "put -P /home/ubuntu/.kube/config /home/ubuntu/.kube/config" > /tmp/batch
+sshpass -p '-veryV3RY53CuRePazWurd!_' sftp -oBatchMode=no -b /tmp/batch ubuntu@$bastion_ip > /tmp/sftp_log 2>&1

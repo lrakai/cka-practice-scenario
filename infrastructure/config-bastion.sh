daf2531bc831adb6a22932b1050209c81a270f1f
Logan Rakai <lmrakai@gmail.com>
2019-05-11 00:28:27 -0600

    Remove sftp source path

--- infrastructure/config-bastion.sh
+++ infrastructure/config-bastion.sh
@@ -3,4 +3,4 @@ apt install sshpass
 bastion_ip=$(aws ec2 describe-instances --region us-west-2 --filters "Name=tag:Name,Values=bastion*" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 ssh-keyscan $bastion_ip >> ~/.ssh/known_hosts
 echo "put -P /home/ubuntu/.kube/config /home/ubuntu/.kube/config" > batch
-sshpass -p '-veryV3RY53CuRePazWurd!_' sftp -oBatchMode=no -b batch ubuntu@$bastion_ip:/home/ubuntu/.kube/config
+sshpass -p '-veryV3RY53CuRePazWurd!_' sftp -oBatchMode=no -b batch ubuntu@$bastion_ip

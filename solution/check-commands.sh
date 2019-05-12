8e4ebf43697b02049f6a1013141ded61e3ba5606
Logan Rakai <lmrakai@gmail.com>
2019-05-12 00:59:36 -0600

    Add varying cpu pods

--- solution/check-commands.sh
+++ solution/check-commands.sh
@@ -7,5 +7,7 @@ if [ $replicas -gt 0 -a $image -gt 0 -a $revisionHistory -gt 0 ]; then echo -n T
 
 # Check service
 svc_selector=$(kubectl --kubeconfig=/home/ubuntu/.kube/config get service -n pwn sitelb -o jsonpath="{.spec.selector}") ;
-deploy_selector=$(kubectl --kubeconfig=/home/ubuntu/.kube/config get deployment -n pwn site -o jsonpath='{.spec.selector.matchLabels}') ;
+deploy_selector=$(kubectl --kubeconfig=/home/ubuntu/.kube/config get deployment -n pwn site -o jsonpath="{.spec.selector.matchLabels}") ;
 if [ "$svc_selector" = "$deploy_selector" ]; then echo -n True; else echo -n False; fi ;
+
+# Check highest cpu

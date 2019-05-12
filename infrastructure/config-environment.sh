e79f820a92bd20ab65f710d303f1c68c0e11fdf9
Logan Rakai <lmrakai@gmail.com>
2019-05-11 23:56:57 -0600

    specify kubeconfig and remove carriage returns

--- infrastructure/config-environment.sh
+++ infrastructure/config-environment.sh
@@ -2,13 +2,13 @@ sudo -i -u ubuntu
 log_file=/tmp/vxioprep
 
 # Check 2 - Repair service
-kubectl create namespace pwn >> $log_file 2>&1
-kubectl create -n pwn deployment --image=httpd:2.4.39-alpine site >> $log_file 2>&1
-kubectl scale -n pwn deployment --replicas=2 site >> $log_file 2>&1
-kubectl create service loadbalancer -n pwn --tcp=80 sitelb >> $log_file 2>&1
+kubectl --kubeconfig=/home/ubuntu/.kube/config create namespace pwn >> $log_file 2>&1
+kubectl --kubeconfig=/home/ubuntu/.kube/config create -n pwn deployment --image=httpd:2.4.39-alpine site >> $log_file 2>&1
+kubectl --kubeconfig=/home/ubuntu/.kube/config scale -n pwn deployment --replicas=2 site >> $log_file 2>&1
+kubectl --kubeconfig=/home/ubuntu/.kube/config create service loadbalancer -n pwn --tcp=80 sitelb >> $log_file 2>&1
 
 # Check 1 - Create specified deployment
-kubectl create namespace cal >> $log_file 2>&1
+kubectl --kubeconfig=/home/ubuntu/.kube/config create namespace cal >> $log_file 2>&1
 
 # Check 3 - Highest CPU pod in namespace
 

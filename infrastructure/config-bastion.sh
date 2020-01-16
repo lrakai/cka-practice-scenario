43207fd2b7b22cc69c3b6e1dbaef7cd18f42cf06
lrakai <lmrakai@gmail.com>
2020-01-16 11:59:19 -0700

    Avoid lenghty apt updates, Improve debugability

--- infrastructure/config-bastion.sh
+++ infrastructure/config-bastion.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
 # Inject kubeconfig
 env > /tmp/env
+
+# stop any auto-update that can block apt install
+systemctl stop apt-daily.service
+systemctl kill --kill-who=all apt-daily.service
+
+# wait until `apt-get updated` has been killed
+while ! (systemctl list-units --all apt-daily.service | egrep -q '(dead|failed)')
+do
+  sleep 1;
+done
+pkill /usr/lib/apt/apt.systemd.daily
+
+sudo apt update
 apt-get install sshpass > /tmp/install_log 2>&1
-bastion_ip=$(aws ec2 describe-instances --region us-west-2 --filters "Name=tag:Name,Values=bastion*" "Name=instance-state-name,Values=pending,running" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+bastion_ip=$(aws ec2 describe-instances --region us-west-2 --filters 'Name=tag:Name,Values=bastion*' 'Name=instance-state-name,Values=running,pending' --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 echo $bastion_ip > /tmp/bastion_ip
+ssh-keyscan $bastion_ip >> ~/.ssh/known_hosts
 echo "put -P /home/ubuntu/.kube/config /home/ubuntu/.kube/config" > /tmp/batch
 sed -i "s/kubernetes-admin@kubernetes/challenge-context/g" /home/ubuntu/.kube/config
 sshpass -p '-veryV3RY53CuRePazWurd!_' sftp -oBatchMode=no -oStrictHostKeyChecking=no -b /tmp/batch ubuntu@$bastion_ip > /tmp/sftp_log 2>&1

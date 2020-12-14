2b8e108c772b6dd7c5a6c6f5dace122f31a91be5
lrakai <lmrakai@gmail.com>
2020-12-13 22:21:26 -0700

    Remove unnecessary apt-daily kill

--- scripts/config-bastion.sh
+++ scripts/config-bastion.sh
@@ -3,17 +3,6 @@
 # Inject kubeconfig
 env > /tmp/env
 
-# stop any auto-update that can block apt install
-systemctl stop apt-daily.service
-systemctl kill --kill-who=all apt-daily.service
-
-# wait until `apt-get updated` has been killed
-while ! (systemctl list-units --all apt-daily.service | egrep -q '(dead|failed)')
-do
-  sleep 1;
-done
-pkill /usr/lib/apt/apt.systemd.daily
-
 sudo apt update
 apt-get install sshpass > /tmp/install_log 2>&1
 bastion_ip=$(aws ec2 describe-instances --region us-west-2 --filters 'Name=tag:Name,Values=bastion*' 'Name=instance-state-name,Values=running,pending' --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
